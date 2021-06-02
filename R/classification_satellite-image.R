585f4f5ae04101a5a0e980cbe9cc0cd58f86509e
Michael Rustler <michael.rustler@kompetenz-wasser.de>
2021-06-03 00:25:23 +0200

    Run styler::style_pkg() and lintr::lint_package()

--- R/classification_satellite-image.R
+++ R/classification_satellite-image.R
@@ -1,4 +1,5 @@
-# image segmentation for urban surfaces with random forest based on spectral signatures
+# image segmentation for urban surfaces with random forest based on spectral
+# signatures
 
 #' build image classification model (random forest)
 #' @description Wrapper function for fitting a random forest using a multi-band
@@ -26,13 +27,13 @@
 #' @param modelName File name for saving the fitted random forest model
 #' @param nCores  no. of cores for running in parallel mode (uses library
 #' 'doParallel'), (default: parallel::detectCores() - 1)
-#' @param mtryGrd Number of trees to grow. In the random forests literature, this
-#' is referred to as the ntree parameter. Larger number of trees produce more
-#' stable models and covariate importance estimates, but require more memory and
-#' a longer run time. For small datasets, 50 trees may be sufficient. For larger
-#' datasets, 500 or more may be required. Please consult the random forests
-#' literature for extensive discussion of this parameter (e.g. Cutler et al., 2007;
-#' Strobl et al., 2007; Strobl et al., 2008).
+#' @param mtryGrd Number of trees to grow. In the random forests literature,
+#' this is referred to as the ntree parameter. Larger number of trees produce
+#' more stable models and covariate importance estimates, but require more
+#' memory and a longer run time. For small datasets, 50 trees may be sufficient.
+#' For larger datasets, 500 or more may be required. Please consult the random
+#' forests literature for extensive discussion of this parameter (e.g. Cutler et
+#' al., 2007; Strobl et al., 2007; Strobl et al., 2008).
 #' @param ntreeGrd Number of variables available for splitting at each tree node.
 #' In the random forests literature, this is referred to as the mtry parameter.
 #' There is extensive discussion in the literature about the influence of mtry.
@@ -47,8 +48,8 @@
 #' @param cvrepeats number of repeats in repeated cross validation (caret),
 #' (default: 2)
 #'
-#' @return Writes spectralSignatures (if overlayExists is FALSE) and fitted random
-#' forest model with name modelName.
+#' @return Writes spectralSignatures (if overlayExists is FALSE) and fitted
+#' random forest model with name modelName.
 #' @export
 #' @importFrom caret createFolds train trainControl
 #' @importFrom doParallel registerDoParallel
@@ -59,11 +60,13 @@
 buildClassMod <- function(rawdir,
                           image,
                           groundTruth,
-                          groundTruthValues = list('roof' = 1,
-                                                   'street' = 2,
-                                                   'pervious' = 3,
-                                                   'shadow' = 4,
-                                                   'water' = 5),
+                          groundTruthValues = list(
+                            "roof" = 1,
+                            "street" = 2,
+                            "pervious" = 3,
+                            "shadow" = 4,
+                            "water" = 5
+                          ),
                           overlayExists = FALSE,
                           spectrSigName,
                           modelName,
@@ -72,44 +75,45 @@ buildClassMod <- function(rawdir,
                           ntreeGrd,
                           nfolds = 3,
                           nodesize = 3,
-                          cvrepeats = 2){
+                          cvrepeats = 2) {
 
   # load image and ground truth data
-  cat('\nloading spatial data...')
+  cat("\nloading spatial data...")
   img <- raster::brick(file.path(rawdir, image))
   ground <- raster::shapefile(file.path(rawdir, groundTruth))
 
   ground@data$cover <- kwb.utils::multiSubstitute(
-    ground@data$cover, groundTruthValues)
+    ground@data$cover, groundTruthValues
+  )
 
-  cat('\ndone\n')
+  cat("\ndone\n")
 
-  if(overlayExists){
-
-    cat('\nreading overlay object with spectral signatures of groundtruth areas...')
+  if (overlayExists) {
+    cat("\nreading overlay object with spectral signatures of groundtruth areas...")
     load(file.path(rawdir, spectrSigName))
-    cat('\ndone\n')
-
+    cat("\ndone\n")
   } else {
 
     # extract spectral values of ground truth areas
-    cat('\nextracting spectral signatures of ground truth areas...')
-    ov <- raster::extract(x=img, y=ground)
-    cat('\ndone\n')
+    cat("\nextracting spectral signatures of ground truth areas...")
+    ov <- raster::extract(x = img, y = ground)
+    cat("\ndone\n")
 
     # save resulting list containing overlay (ov)
-    if(!is.na(spectrSigName))
-      cat('\nsaving overlay object...')
-    save(ov, file=file.path(rawdir, spectrSigName))
-    cat('\ndone\n')
+    if (!is.na(spectrSigName)) {
+      cat("\nsaving overlay object...")
+    }
+    save(ov, file = file.path(rawdir, spectrSigName))
+    cat("\ndone\n")
   }
 
   # convert ov into data.frame for each surface type in ground truth data
   coverTypes <- unique(ground@data$cover)
-  pos <- lapply(X=coverTypes, FUN=function(x) which(ground@data$cover == x))
-  Xi <- lapply(X=pos, FUN=function(x) do.call(rbind, ov[x]))
-  X <- data.frame(do.call(what=rbind, args=Xi),
-                  cover=rep(coverTypes, times=unlist(lapply(Xi, nrow))))
+  pos <- lapply(X = coverTypes, FUN = function(x) which(ground@data$cover == x))
+  Xi <- lapply(X = pos, FUN = function(x) do.call(rbind, ov[x]))
+  X <- data.frame(do.call(what = rbind, args = Xi),
+    cover = rep(coverTypes, times = unlist(lapply(Xi, nrow)))
+  )
   X$cover <- as.factor(X$cover)
 
   # train random forest model, repeated cross-validation, grid search (mtry),
@@ -119,31 +123,39 @@ buildClassMod <- function(rawdir,
   cl <- parallel::makePSOCKcluster(nCores)
   doParallel::registerDoParallel(cl)
 
-  train.control <- caret::trainControl(method="repeatedcv",
-                                       number=nfolds,
-                                       repeats=cvrepeats,
-                                       index=caret::createFolds(y=X$cover,
-                                                                k=nfolds,
-                                                                returnTrain=FALSE,
-                                                                list=TRUE),
-                                       search='grid',
-                                       allowParallel=TRUE)
-  cat('\ntraining model...')
-  model <- caret::train(form=cover ~.,
-                        data=X,
-                        method="rf",
-                        tunegrid=expand.grid(mtry=mtryGrd,
-                                             ntree=ntreeGrd),
-                        nodesize=nodesize,
-                        trControl=train.control)
+  train.control <- caret::trainControl(
+    method = "repeatedcv",
+    number = nfolds,
+    repeats = cvrepeats,
+    index = caret::createFolds(
+      y = X$cover,
+      k = nfolds,
+      returnTrain = FALSE,
+      list = TRUE
+    ),
+    search = "grid",
+    allowParallel = TRUE
+  )
+  cat("\ntraining model...")
+  model <- caret::train(
+    form = cover ~ .,
+    data = X,
+    method = "rf",
+    tunegrid = expand.grid(
+      mtry = mtryGrd,
+      ntree = ntreeGrd
+    ),
+    nodesize = nodesize,
+    trControl = train.control
+  )
 
   parallel::stopCluster(cl)
-  cat('\ndone\n')
+  cat("\ndone\n")
 
   # save model
-  cat('\nsaving model...')
-  save(model, file=file.path(rawdir, modelName))
-  cat('\ndone\n')
+  cat("\nsaving model...")
+  save(model, file = file.path(rawdir, modelName))
+  cat("\ndone\n")
 }
 
 # apply model to predict surface type (roof, street, ...)
@@ -159,25 +171,29 @@ buildClassMod <- function(rawdir,
 #' @export
 #' @importFrom raster brick predict writeRaster
 #'
-predictSurfClass <- function(rawdir, modelName, image, predName){
+predictSurfClass <- function(rawdir, modelName, image, predName) {
 
   # load model object
-  cat('\nloading model...')
+  cat("\nloading model...")
   load(file.path(rawdir, modelName))
-  cat('\ndone\n')
+  cat("\ndone\n")
 
   # load image to be classified
   img <- raster::brick(file.path(rawdir, image))
 
   # predict
-  cat('\nmaking predictions...')
-  pred <- raster::predict(object=img,
-                          model=model$finalModel,
-                          type='class')
-  cat('\ndone\n')
+  cat("\nmaking predictions...")
+  pred <- raster::predict(
+    object = img,
+    model = model$finalModel,
+    type = "class"
+  )
+  cat("\ndone\n")
 
   # write predictions as raster
-  cat('\nwriting output...')
-  raster::writeRaster(pred, filename=file.path(rawdir, predName), overwrite=TRUE)
-  cat('\ndone\n')
+  cat("\nwriting output...")
+  raster::writeRaster(pred,
+                      filename = file.path(rawdir, predName),
+                      overwrite = TRUE)
+  cat("\ndone\n")
 }
